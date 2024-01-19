1132881cef91ad44f9445aa3858ccff3bee45cf5
ednavarrol <carthagoxz@gmail.com>
2024-01-18 22:44:03 -0300

    changed dummy data

--- src/data/index.ts
+++ src/data/index.ts
@@ -11,7 +11,7 @@ export const NavLinkData : NavLink [] = [
     },
     {
         label: "LinkedIn",
-        url: "https://www.linkedin.com/in/sagar-pratap/",
+        url: "#",
     }
 ]
 
@@ -20,14 +20,14 @@ export const ProjectData : Project [] = [
         name: "SQLito",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         image: "sqlito.png",
-        live_url: "https://sqlito.herokuapp.com/",
-        repo_url: "https://github.com/ednavarrol/sqlito",
+        live_url: "#",
+        repo_url: "#",
     },
     {
         name: "KarmApp",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         image: "karma.png",
-        live_url: "https://karma.herokuapp.com/",
-        repo_url: "https://github.com/ednavarrol/karma",
+        live_url: "#",
+        repo_url: "#",
     }
 ]
